d8d26c139cad571167871db3ac99951805efe806
Jaskaran Singh <jaskaranadhen0004@gmail.com>
2024-07-17 22:46:19 +0530

    Initial commit

---
+++ Lecture 1.cpp
@@ -0,0 +1,1 @@
+In this lecture, my mentor provided a brief overview of the course and programming. He emphasized the importance of having patience when working with code. This was the introductory lecture for the Data Structures course using C++. There was no code introduced in this lecture, so I didn't write any code down.
