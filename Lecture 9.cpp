c53cc7e5513efc93dfb1e49f41775dd17e145ab0
Jaskaran Singh <118327514+jaskaran-adhen@users.noreply.github.com>
2024-07-17 23:30:52 +0530

    Add files via upload

---
+++ Lecture 9.cpp
@@ -0,0 +1,26 @@
+// #include <iostream>
+// using namespace std;
+// int main(){
+// 	for (int i = 0;i<5;i++){   
+// 		cout<<i<<endl;
+// 	}
+// 	return 0;
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){
+// 	for(int i=0; i<=100;i++){ 
+// 		cout<<i<<endl;
+// 	}
+// 	return 0;
+// }
+
+#include<iostream>
+using namespace std;
+int main(){
+	for(int i=0; i<100;i=i+2){ 
+		cout<<i<<endl;
+	}
+	return 0;
+}
