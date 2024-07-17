1b5f0199ac76e8e5b31ade5be668f19c13ac9c14
Jaskaran Singh <118327514+jaskaran-adhen@users.noreply.github.com>
2024-07-17 23:28:41 +0530

    Add files via upload

---
+++ Lecture 8.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main(){
+	int array[3]={121,142,125}; 
+	cout<< array[2];
+
+  int a[4] = {1,2,3,4,5,6,7,78,77};
+  for (int i = 0; i < 9; i++) {
+    cout << a[i] << endl;
+  }
+
+	int a[3] = {1,2};
+	for(int i=0;i<2; i++){
+		cout<<i<<"="<<a[i]<<endl;
+	}
+	return 0;
+}
+
