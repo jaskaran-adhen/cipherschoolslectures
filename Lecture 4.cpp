e50d862d8f75156c06a6489ddfdc17663673ec58
Jaskaran Singh <118327514+jaskaran-adhen@users.noreply.github.com>
2024-07-17 23:13:43 +0530

    Add files via upload

---
+++ Lecture 4.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main(){
+	
+	int a,b,c;  //Declare the 3 variables and assign the values. 
+	a=14 ; b=51 ;c=60 ;
+	
+	int d=a + b- c;
+	
+	cout<<"Ans is "<<d;
+	
+	return 0;
+}
