6465295875eb5626c6fa6bfff0411c251a990e6d
Jaskaran Singh <jaskaranadhen0004@gmail.com>
2024-07-18 20:05:03 +0530

    add files

--- Lecture10.cpp
+++ Lecture10.cpp
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
  
 
 #include<iostream>
@@ -8,15 +7,4 @@ int main(){
 		cout<<i<<endl;
 	}
 	return 0;
-=======
- 
-
-#include<iostream>
-using namespace std;
-int main(){
-	for(int i=0; i<=200;i++){ 
-		cout<<i<<endl;
-	}
-	return 0;
->>>>>>> e6bb5af210bcfd289ac4d2b448fd50424be42c7d
 }
